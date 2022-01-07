ec63462549035e81a8f42c3aa9be0ffdb50e7692
pxladin <filipkoch27@gmail.com>
2022-01-07 07:34:08 +0100

    ⚡️ improve code for decrypt-all-authors modifier

--- src/markdown/content/global-modifer/decrypt-all-authors.js
+++ src/markdown/content/global-modifer/decrypt-all-authors.js
@@ -1,9 +1,9 @@
-const Traning = require('../../content/Traning');
-
 module.exports = (ctx) => {
-  ctx.contents.forEach((content) => {
-    if (content instanceof Traning) {
-      ctx.storage.authors[content.authorId - 1].decrypt();
-    }
-  });
+  const { storage } = ctx;
+
+  if (storage.authors) {
+    storage.authors.forEach((author) => {
+      author.decrypt();
+    });
+  }
 };
