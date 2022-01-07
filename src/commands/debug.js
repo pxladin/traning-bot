8e9111f4ac327179bac6aeba1d8022856cfcecc8
pxladin <filipkoch27@gmail.com>
2022-01-07 07:39:10 +0100

    ✨ add speed indicator to debug command

--- src/commands/debug.js
+++ src/commands/debug.js
@@ -1,7 +1,9 @@
 const Context = require('../markdown/Context');
 
 module.exports = (client, { channel, id: messageId, author }, args) => {
+  const startTime = Date.now();
   const ctx = new Context(args.join(' ')).parse();
+  const timeTook = Date.now() - startTime;
 
   ctx.metaData = {
     id: messageId,
@@ -18,5 +20,8 @@ module.exports = (client, { channel, id: messageId, author }, args) => {
     delete c.ctx;
   });
 
-  channel.send(`\`\`\`json\n${JSON.stringify(ctx, null, 2)}\n\`\`\``);
+  channel.send(`
+Took ${timeTook}ms
+\`\`\`json\n${JSON.stringify(ctx, null, 2)}\n\`\`\`
+`);
 };
